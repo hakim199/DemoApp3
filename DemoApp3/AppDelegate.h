628077c20c7514842bf4aa11b3165323ab1c9eca
Hakim <hakim@Hakims-MacBook-Pro.local>
2015-10-16 19:38:34 -0400

    initial app3 commit

---
+++ DemoApp3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoApp3
+//
+//  Created by Hakim on 10/16/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoApp3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoApp3
+//
+//  Created by Hakim on 10/16/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
